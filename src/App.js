5a3f3b283e1a36397f4cfd9db8c6b95de9c831e7
vishal895 <vishal9389083502@gmail.com>
2024-05-14 11:57:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>dsfjddls</h1>
+      <h2>vishal</h2>
+      <h2>srivastava</h2>
+    </div>
+  );
+}
+
+export default App;
